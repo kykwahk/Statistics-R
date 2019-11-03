5862f76e8a0e1945eb73b8be19dfb98883e9ff10
kykwahk <kykwahk@gmail.com>
2019-11-03 19:17:57 +0900

    Chapter 10

---
+++ Statistics-R10-R을이용한통계데이터분석.R
@@ -0,0 +1,140 @@
+##################################
+## CHAPTER 10. Cluster Analysis ##
+##################################
+
+##############################
+## 10.1 Similarity Measures ##
+##############################
+
+install.packages("flexclust")
+library(flexclust)
+data(nutrient)
+head(nutrient, 5)
+
+d <- dist(nutrient)
+class(d)
+labels(d)
+as.matrix(d)[1:5, 1:5]
+
+########################################
+## 10.2 Hierarchical Cluster Analysis ##
+########################################
+
+library(flexclust)
+data(nutrient)
+nutrition <- nutrient
+row.names(nutrition) <- tolower(row.names(nutrition))
+nutrition.scaled <- scale(nutrition)
+
+d <- dist(nutrition.scaled)
+clustering.average <- hclust(d, method="average")
+
+# [Figure 10-1]
+windows(width=7.0, height=5.5)
+plot(clustering.average, hang=-1, cex=0.9, col="darkgreen",
+     xlab="Food", main="Hierarchical Clustering with Average Linkage")
+
+install.packages("NbClust")
+library(NbClust)
+nc <- NbClust(nutrition.scaled, distance="euclidean",
+              min.nc=3, max.nc=15, method="average")
+
+str(nc)
+nc$Best.nc
+
+table(nc$Best.nc[1,])
+
+# [Figure 10-2]
+windows(width=7.0, height=5.5)
+barplot(table(nc$Best.nc[1,]), col="mistyrose", 
+        xlab="Number of Cluster", ylab="Number of Supporting Index",
+        main="Number of Clusters Proposed by Indices")
+
+clusters <- cutree(clustering.average, k=5)
+clusters
+
+table(clusters)
+
+# [Figure 10-3]
+windows(width=7.0, height=5.5)
+plot(clustering.average, hang=-1, cex=0.9, col="darkgreen",
+     xlab="Food", 
+     main="Hierarchical Clustering with Average Linkage\n Five Clusters")
+rect.hclust(clustering.average, k=5)
+
+aggregate(nutrition, by=list(cluster=clusters), mean)
+
+a <- aggregate(nutrition.scaled, by=list(cluster=clusters), mean)
+n <- as.vector(table(clusters))
+cbind(a, n)
+
+########################################
+## 10.3 Partitioning Cluster Analysis ##
+########################################
+
+## k-means
+
+head(state.x77)
+state.scaled <- scale(state.x77)
+
+library(NbClust)
+set.seed(123)
+nc <- NbClust(state.scaled, distance="euclidean", 
+              min.nc=2, max.nc=15, method="kmeans")
+table(nc$Best.nc[1,])
+
+# [Figure 10-4]
+windows(width=7.0, height=5.5)
+barplot(table(nc$Best.nc[1,]), col="lightsteelblue",
+        xlab="Number of Cluster", ylab="Number of Supporting Index",
+        main="Number of Clusters Proposed by Indices")
+
+set.seed(123)
+clustering.km <- kmeans(state.scaled, centers=3, nstart=25)
+
+str(clustering.km)
+
+clustering.km$cluster
+clustering.km$centers
+clustering.km$size
+
+aggregate(state.x77, by=list(cluster=clustering.km$cluster), mean)
+
+# [Figure 10-5]
+windows(width=7.0, height=5.5)
+library(cluster)
+clusplot(x=state.x77, clus=clustering.km$cluster, color=TRUE, shade=TRUE,
+         labels=2, lines=0, main="Cluster Plot")
+
+## PAM
+
+install.packages("rattle")
+library(rattle)
+head(wine)
+
+library(cluster)
+set.seed(123)
+clustering.pam <- pam(wine[-1], k=3, stand=TRUE)
+
+str(clustering.pam) 
+
+clustering.pam$clusinfo
+
+clustering.pam$medoids
+clustering.pam$id.med
+
+clustering.pam$clustering
+aggregate(wine[-1], by=list(cluster=clustering.pam$clustering), mean)
+
+# [Figure 10-6]
+windows(width=7.0, height=5.5)
+clusplot(clustering.pam, color=TRUE, shade=TRUE, labels=4, lines=0, 
+         main="Cluster Plot")
+
+result.pam <- table(wine$Type, clustering.pam$clustering, 
+                    dnn=c("Actual", "Clustered"))
+result.pam
+mean(wine$Type==clustering.pam$clustering)
+
+library(flexclust)
+randIndex(result.pam)
